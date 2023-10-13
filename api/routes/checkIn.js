9668835a8a8ab4c31e1b99b7a87a37c20a5d35dc
nmemarcoding <nmemarcoding@gmail.com>
2023-10-13 11:38:46 -0700

    Populating the user's first name and last name.

--- api/routes/checkIn.js
+++ api/routes/checkIn.js
@@ -2,36 +2,46 @@ const router = require('express').Router();
 const CheckIn = require('../models/checkIn');
 const User = require('../models/user');
 
-
-// rout to check the member in
-
+// Route to check a member in
 router.post('/', async (req, res) => {
-    // find user with phone number or membeship number
+    // Validate incoming data
+    if (!req.body.phoneNumber && !req.body.membershipNumber) {
+        return res.status(400).send('Phone number or membership number is required.');
+    }
 
-    const user = await User.findOne({ $or: [{ phoneNumber: req.body.phoneNumber }, { membershipNumber: req.body.membershipNumber }] });
-    if (!user) return res.status(400).send('User not found');
-    // check if user membership is active
-    if (user.membershipExpiration < Date.now()) return res.status(400).send('Membership expired');
-    
-    // create checkin try and catch
     try {
+        // Find user with phone number or membership number
+        const user = await User.findOne({
+            $or: [{ phoneNumber: req.body.phoneNumber }, { membershipNumber: req.body.membershipNumber }]
+        }).populate('firstName lastName');
+
+        if (!user) {
+            return res.status(400).send('User not found');
+        }
+
+        // Check if user membership is active
+        if (user.membershipExpiration < Date.now()) {
+            return res.status(400).send('Membership expired');
+        }
+
+        // Populate firstName and lastName
+        
+
+        // Create a check-in record
         const checkIn = new CheckIn({
             user: user._id,
         });
         await checkIn.save();
-        res.send(checkIn);
-     
+
+        // Send only firstName and lastName in the response
+        res.send({
+            firstName: user.firstName,
+            lastName: user.lastName,
+        });
     } catch (error) {
-        res.status(400).send(error);
-  
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-
-}
-);
-  
-
-
-
-
+});
 
 module.exports = router;
