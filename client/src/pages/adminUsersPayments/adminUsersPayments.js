156cf23a79238a4b731538634520eb6282c65a2a
nmemarcoding <nmemarcoding@gmail.com>
2023-10-18 19:37:08 -0700

    Search functionality to search through payments by user first and last name.'

--- client/src/pages/adminUsersPayments/adminUsersPayments.js
+++ client/src/pages/adminUsersPayments/adminUsersPayments.js
@@ -6,6 +6,7 @@ import useAdminAuth from '../../hooks/adminAuth.js';
 export default function MemberPaymentHistory() {
     useAdminAuth();
     const [payments, setPayments] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         publicRequest().get('/payment')
@@ -15,8 +16,14 @@ export default function MemberPaymentHistory() {
             .catch((error) => {
                 console.log(error);
             });
-    }
-        , []);
+    }, []);
+
+    const filteredPayments = payments.filter(payment => {
+    const fullName = `${payment.userId?.firstName.toLowerCase()} ${payment.userId?.lastName.toLowerCase()}`;
+    return fullName.includes(searchTerm.toLowerCase()) || 
+           payment.userId?.firstName.toLowerCase().includes(searchTerm.toLowerCase()) || 
+           payment.userId?.lastName.toLowerCase().includes(searchTerm.toLowerCase());
+});
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -24,6 +31,17 @@ export default function MemberPaymentHistory() {
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 <div className="px-4 py-6 sm:px-0">
                     <h1 className="text-3xl font-bold text-gray-900 mb-4">Payment History</h1>
+                    <div className="flex flex-col md:flex-row mb-4">
+                        <div className="md:w-2/3 mb-2 md:mb-0">
+                            <input
+                                type="text"
+                                placeholder="Search by first or last name"
+                                className="w-full border border-gray-300 p-2 rounded-lg"
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                        </div>
+                    </div>
                     <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table className="min-w-full divide-y divide-gray-200">
                             <thead className="bg-gray-50">
@@ -43,7 +61,7 @@ export default function MemberPaymentHistory() {
                                 </tr>
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
-                                {payments.map(payment => (
+                                {filteredPayments.map(payment => (
                                     <tr key={payment._id}>
                                         <td className="px-6 py-4 whitespace-nowrap hidden sm:table-cell">
                                             <div className="text-sm text-gray-900">{new Date(payment.date).toLocaleDateString()}</div>
