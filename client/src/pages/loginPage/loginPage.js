0e587d5810be922e86a7ac3d492f6bc450f34c39
nmemarcoding <nmemarcoding@gmail.com>
2023-11-01 19:29:07 -0700

    loading stage added

--- client/src/pages/loginPage/loginPage.js
+++ client/src/pages/loginPage/loginPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { publicRequest } from '../../hooks/requestMethods';
 import { useNavigate } from 'react-router-dom';
 import useStore from '../../store';
+
 export default function LoginPage() {
   const addUserInfo = useStore((state) => state.addUserInfo);
   const navigate = useNavigate();
@@ -10,6 +11,7 @@ export default function LoginPage() {
     email: '',
     password: ''
   });
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleInputChange = (event) => {
     const { id, value } = event.target;
@@ -21,23 +23,23 @@ export default function LoginPage() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setIsLoading(true);
     
     publicRequest().post('auth/login', formData)
       .then((response) => {
-        console.log(response);
         alert('Logged in successfully');
         addUserInfo(response.data);
         if(response.data.isAdmin === true){
           navigate('/admindashboard');
-          
-        }else{
+        } else {
           navigate('/');
         }
-        
       })
       .catch((error) => {
-        console.log(error);
         alert(error.response.data || 'An error occurred');
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
@@ -77,16 +79,37 @@ export default function LoginPage() {
         <div className="flex items-center justify-center mt-6 sm:mt-8 mb-4 sm:mb-8">
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150"
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ${isLoading ? 'cursor-not-allowed' : ''}`}
+            disabled={isLoading}
           >
-            Log In
+            {isLoading ? (
+              <>
+                <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+               
+              </>
+            ) : (
+              'Log In'
+            )}
           </button>
         </div>
 
         <div className="text-gray-700 font-bold mb-2 block">
           Don't have an account? <Link to="/signup" className="text-blue-600 hover:text-blue-800">Sign up</Link>
         </div>
-
       </form>
     </div>
   )
