edd3256a7ec139527b0e786a4cf68c22df579a1c
nmemarcoding <nmemarcoding@gmail.com>
2023-11-01 19:37:43 -0700

    loading animation added to the page

--- client/src/pages/paymenthistory/paymenthistory.js
+++ client/src/pages/paymenthistory/paymenthistory.js
@@ -8,20 +8,22 @@ export default function PaymentHistory() {
     useAuthRedirect();
     const userInfo = store.getState().userInf;
     const [payments, setPayments] = useState([]);
+    const [loading, setLoading] = useState(false); // Loading state
 
-
-    // Add the hard-coded payment data to the existing payments state
     useEffect(() => {
+        setLoading(true); // Set loading to true before making the request
         publicRequest().get(`/payment/${userInfo._id}`)
             .then((response) => {
                 setPayments(response.data.reverse());
             })
             .catch((error) => {
                 console.log(error);
+            })
+            .finally(() => {
+                setLoading(false); // Set loading to false once request is completed
             });
     }, []);
 
-    // Format the date as "YYYY-MM-DD"
     const formatDate = (date) => {
         const d = new Date(date);
         const year = d.getFullYear();
@@ -37,46 +39,52 @@ export default function PaymentHistory() {
                 <div className="px-4 py-6 sm:px-0">
                     <h1 className="text-3xl font-bold text-gray-900">Payment History</h1>
                     <div className="mt-6">
-                        <div className="flex flex-col">
-                            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                        <table className="min-w-full divide-y divide-gray-200">
-                                            <thead className="bg-gray-50">
-                                                <tr>
-                                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                        Date
-                                                    </th>
-                                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                        Amount
-                                                    </th>
-                                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                        Status
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody className="bg-white divide-y divide-gray-200">
-                                                {payments.map(payment => (
-                                                    <tr key={payment._id}>
-                                                        <td className="px-6 py-4 whitespace-nowrap">
-                                                            <div className="text-sm text-gray-900">{formatDate(payment.date)}</div>
-                                                        </td>
-                                                        <td className="px-6 py-4 whitespace-nowrap">
-                                                            <div className="text-sm text-gray-900">{payment.amount}</div>
-                                                        </td>
-                                                        <td className="px-6 py-4 whitespace-nowrap">
-                                                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                                Paid
-                                                            </span>
-                                                        </td>
+                        {loading ? ( // Display loading animation based on loading state
+                            <div className="text-center py-4 text-2xl font-bold text-gray-500 animate-pulse">
+                                Loading...
+                            </div>
+                        ) : (
+                            <div className="flex flex-col">
+                                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                            <table className="min-w-full divide-y divide-gray-200">
+                                                <thead className="bg-gray-50">
+                                                    <tr>
+                                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            Date
+                                                        </th>
+                                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            Amount
+                                                        </th>
+                                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            Status
+                                                        </th>
                                                     </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
+                                                </thead>
+                                                <tbody className="bg-white divide-y divide-gray-200">
+                                                    {payments.map(payment => (
+                                                        <tr key={payment._id}>
+                                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                                <div className="text-sm text-gray-900">{formatDate(payment.date)}</div>
+                                                            </td>
+                                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                                <div className="text-sm text-gray-900">{payment.amount}</div>
+                                                            </td>
+                                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                                    Paid
+                                                                </span>
+                                                            </td>
+                                                        </tr>
+                                                    ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
